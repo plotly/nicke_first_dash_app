8c41d279b19df1e7adfb35c9e3194d1701af7b21
Nicholas Esterer <nicholas.esterer@plot.ly>
2020-05-06 17:11:00 -0400

    We can serve an image straight from bytes in memory
    so that's good

---
+++ test/simple_flask_img_server.py
@@ -0,0 +1,22 @@
+# Study to see how to serve an image using flask
+
+import flask
+import PIL.Image
+import plotly
+from plotly.utils import ImageUriValidator
+import pdb
+import io
+import base64
+
+server=flask.Flask(__name__)
+
+# pil_image_to_uri seems to encode as png always (currently)
+@server.route('/dl_image.png')
+def provide_image():
+    imgf = PIL.Image.open("assets/Keymaker.jpg")
+    uri=ImageUriValidator.pil_image_to_uri(imgf)
+    mime,contents=uri.split(';')
+    typ,cont=contents.split(',')
+    byt=base64.b64decode(cont)
+    return flask.send_file(io.BytesIO(byt),mimetype=mime)
+
